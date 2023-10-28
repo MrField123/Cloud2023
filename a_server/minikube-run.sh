aeb415a07135d4c4bbdb83573a92b6f6b01f6394
Jonas A <41610768+MrField123@users.noreply.github.com>
2023-10-28 22:58:40 +0200

    microservice to get an voucher

--- a_server/minikube-run.sh
+++ a_server/minikube-run.sh
@@ -6,7 +6,7 @@ docker build -t my-super-web-app .
 
 # Create Kubernetes resources
 kubectl apply -f mariadb-deployment.yaml
-#kubectl apply -f 02-k8s-mariadb-service.yaml
+kubectl apply -f k8s-mariadb-service.yaml
 #kubectl apply -f 03-memcached-deployment-and-service.yaml
-#kubectl apply -f 04-app-deployment-minikube.yaml
+kubectl apply -f app-deployment-minikube.yaml
 #kubectl apply -f 04-app-service-and-ingress.yaml
--- a_server/server.js
+++ a_server/server.js
@@ -60,7 +60,7 @@ app.use(prometheusBundle({
 
 // Redirect / to person with ID l.mlb.com - p.7491
 app.get('/', function (request, response) {
-	response.(302, { 'Location': 'getvoucher/1' })
+	response.writeHead(302, { 'Location': 'getvoucher/1' })
 	response.end()
 })
 
