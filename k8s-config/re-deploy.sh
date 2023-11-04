01e35549c593fff91d18e009c3ef27810e2845b5
Jonas A <41610768+MrField123@users.noreply.github.com>
2023-11-04 16:08:08 +0100

    .

--- k8s-config/re-deploy.sh
+++ k8s-config/re-deploy.sh
@@ -20,13 +20,19 @@ kubectl delete svc/voucher-hub-service
 kubectl delete svc/my-app-mariadb-service
 kubectl delete svc/my-super-app-service
 
+kubectl delete ingress my-super-app-ingress
+
 # Create Kubernetes resources
 kubectl apply -f app-deployment-frontend.yaml
 kubectl apply -f voucher-hub-service.yaml
 kubectl apply -f mariadb-deployment.yaml
 kubectl apply -f k8s-mariadb-service.yaml
 kubectl apply -f app-deployment-minikube.yaml
-kubectl apply -f microservice-service.yamlkubectl run -i --tty --rm debug --image=curlimages/curl --restart=Never
+kubectl apply -f microservice-service.yaml
+kubectl apply -f ingress.yaml
+
+
+#kubectl run -i --tty --rm debug --image=curlimages/curl --restart=Never
 
 
 
--- services/init.sql
+++ services/init.sql
@@ -1,4 +1,3 @@
--- Neue Tabelle anlegen
 CREATE TABLE `vouchers` (
   `id` int(11) NOT NULL auto_increment,
   `type` varchar(100) default NULL,
@@ -9,6 +8,5 @@ CREATE TABLE `vouchers` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- Demodaten in neue Tabelle füllen
 INSERT INTO `vouchers` (`type`,`code`,`value`,`name`,`valid`) VALUES ('money','QRCode1','10','Max Muster',1);
 INSERT INTO `vouchers` (`type`,`code`,`value`,`name`,`valid`) VALUES ('money','QRCode2','20','Max Muster 2',1);
