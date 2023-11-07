8fe788bf8bbd48e7d614c665ee15361c66a74bdd
Jonas A <41610768+MrField123@users.noreply.github.com>
2023-11-07 21:44:55 +0100

    update DB schema

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `vouchers` (
   `id` int(11) NOT NULL auto_increment,
-  `type` varchar(100) default NULL,
-  `code` varchar(100) default NULL,
+  `type` varchar(100) NOT NULL,
+  `code` varchar(100) NOT NULL,
   `value` float(4,2) NOT NULL,
-  `name` varchar(100) default NULL,
-  `valid` boolean default NULL,
+  `name` varchar(100) NOT NULL,
+  `valid` boolean NOT NULL default 1,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
